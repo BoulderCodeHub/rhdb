090815df63735cccfc9d87063f2af5db95a5fcdc
Alan Butler <rabutler@usbr.gov>
2024-01-03 14:07:41 -0700

    updated tests for now encluding lc metadata
    added by #6de846c2234881cefe40cea72409987de76d9c5b

--- tests/testthat/test-hdb_metadata.R
+++ tests/testthat/test-hdb_metadata.R
@@ -1,5 +1,5 @@
 test_that("hdb_metadata() errors correctly", {
-  expect_error(hdb_metadata(server = "lc"))
+  expect_error(hdb_metadata(server = "ecao"))
   expect_error(hdb_metadata(type = "gage"))
   expect_error(hdb_metadata(c("uc", "lc")))
   expect_error(hdb_metadata(type = c("reservoir", "gage")))
